fe6d617049e32ec3e80971843ecbeb71694717bd
Walter Lopez <darwin010cr2@gmail.com>
2017-09-27 13:58:57 -0700

    passowwrds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'chatrooms#index'
+  
+  get '/chatrooms/', to: 'chatrooms#new'
+  
+  resources :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
